c2c47e4af3d232c3b9376133a65fb5bfb8d88102
Pedrokk <pedro.tristao@etec.sp.gov.br>
2025-02-25 12:23:51 -0300

    Codigo em java

--- src/ClassePincipal.java
+++ src/ClassePincipal.java
@@ -2,12 +2,30 @@
 public class ClassePincipal {
 
 	public static void main(String[] args) {
+		Pessoa objetoPedro = new Pessoa();
 		
-		//variável para guardar idade!
-		int idade = 18;
+		objetoPedro.atributoNome = "Pedro";
+		objetoPedro.atributoIdade = 24;
+		objetoPedro.atributoRenda = 1000.00f;
+		objetoPedro.atributoTrabalhando = true;
 		
-		int ano = 2025;
-
+		
+		Pessoa objetoMatheus = new Pessoa();
+		
+		objetoMatheus.atributoNome = "Matheus";
+		objetoMatheus.atributoIdade = 22;
+		objetoMatheus.atributoRenda = 1000.00f;
+		objetoMatheus.atributoTrabalhando = true;
+		
+		
+		System.out.println(objetoPedro.atributoNome);
+		System.out.println(objetoMatheus.atributoNome);
+		
+		int variavel = objetoPedro.enviarNumero();
+		
+		
+		objetoPedro.imprimirValor();
+		
 	}
 
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,16 @@
+
+public class Pessoa {
+	int atributoIdade;
+	String atributoNome;
+	boolean atributoTrabalhando;
+	float atributoRenda;
+	
+	
+	public void imprimirValor() {
+		System.out.println();
+	}
+	
+	public int enviarNumero() {
+		return 7;
+	}
+}
